ff3da574ac06a4d48231c95d56125ade56a071e8
Joe <joekosten@gmail.com>
2016-11-07 14:58:50 -0500

    Movie class and MovieIO class created. No matter which genre is entered, all 100 movies are displayed.

---
+++ ConsoleLab10(MovieList)/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLab10_MovieList_
+{
+  public class Movie
+    {
+        public Movie(string name, string genre)
+        {
+            title = name;
+            category = genre;
+        }
+        private string title;
+
+        public string name
+        {
+            get { return title; }
+        }
+        private string category;
+
+        public string genre
+        {
+            get { return category; }
+        }
+    }
+}
--- ConsoleLab10(MovieList)/Program.cs
+++ ConsoleLab10(MovieList)/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleLab10_MovieList_
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Movie List Application!" + Environment.NewLine);
+            Console.WriteLine("The List contains movies from the following categories:");
+            Console.WriteLine("(animated, drama, horror, and scifi)" + Environment.NewLine);
+            Console.Write("Which category are you interested in? ");
+
+            string genre = Console.ReadLine();
+
+            List<Movie> MovieList = new List<Movie>() { };
+            MovieList = MovieIO.MovieCatalog;
+
+            foreach (Movie m in MovieList) {
+                Console.WriteLine(m.name);
+            }
         }
     }
 }
